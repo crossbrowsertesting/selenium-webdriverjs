95eddbe32e400dca53ed66cd5de98c258d31c666
Luke Kende <luke.kende@gmail.com>
2016-09-14 12:47:23 -0600

    new login form example

---
+++ login-form.js
@@ -0,0 +1,181 @@
+// Login Form - Selenium Example Script
+// see https://github.com/SeleniumHQ/selenium/wiki/WebDriverJs for details
+// runs test against http://crossbrowsertesting.github.io/login-form.html
+
+var webdriver = require('selenium-webdriver');
+var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
+var request = require('request');
+var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
+
+var username = 'user@email.com'; //replace with your email address 
+var authkey = '12345'; //replace with your authkey  
+
+var capabilities = {
+    name : 'Login Form - Selenium Test Example',
+    build :  '1.0',
+    browser_api_name : 'Chrome53', 
+    os_api_name : 'Win8', 
+    screen_resolution : '1024x768',
+    record_video : "true",
+    record_network : "true",
+    browserName : "chrome", // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
+    username : username,
+    password : authkey
+};
+
+var sessionId = null;
+
+//register general error handler
+webdriver.promise.controlFlow().on('uncaughtException', webdriverErrorHandler);
+
+console.log('Connection to the CrossBrowserTesting remote server');
+
+var driver = new webdriver.Builder()
+            .usingServer(remoteHub)
+            .withCapabilities(capabilities)
+            .build();
+
+//console.log('driver is ', driver)
+
+
+
+// All driver calls are automatically queued by flow control.
+// Async functions outside of driver can use call() function.
+// To get a feel for the order of execution the console.logs happen after each command is actually run
+console.log('Waiting on the browser to be launched and the session to start');
+
+driver.getSession().then(function(session){
+    sessionId = session.id_; //need for API calls
+    console.log('Session ID: ', sessionId); 
+    console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
+});
+
+//load your URL
+driver.get('http://crossbrowsertesting.github.io/login-form.html');
+
+//take snapshot via cbt api
+driver.call(takeSnapshot);
+
+ //find checkout and click it 
+driver.findElement(webdriver.By.id("username")).sendKeys("tester@crossbrowsertesting.com");
+
+//send keys to element to enter text
+driver.findElement(webdriver.By.xpath("//*[@type=\"password\"]")).sendKeys("test123");
+
+//take snapshot via cbt api
+driver.call(takeSnapshot);
+
+//click the archive button
+driver.findElement(webdriver.By.css("button[type=submit]")).click();
+
+//wait on logged in message
+driver.wait(webdriver.until.elementLocated(webdriver.By.id("logged-in-message")), 10000);
+
+//take snapshot via cbt api
+driver.call(takeSnapshot);
+
+//quit the driver
+driver.quit()
+
+//set the score as passing
+driver.call(setScore, null, 'pass').then(function(result){
+    console.log('set score to pass')
+});
+
+
+//Call API to set the score
+function setScore(score) {
+
+    //webdriver has built-in promise to use
+    var deferred = webdriver.promise.defer();
+    var result = { error: false, message: null }
+
+    if (sessionId){
+        
+        request({
+            method: 'PUT',
+            uri: 'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId,
+            body: {'action': 'set_score', 'score': score },
+            json: true
+        },
+        function(error, response, body) {
+            if (error) {
+                result.error = true;
+                result.message = error;
+            }
+            else if (response.statusCode !== 200){
+                result.error = true;
+                result.message = body;
+            }
+            else{
+                result.error = false;
+                result.message = 'success';
+            }
+
+            deferred.fulfill(result);
+        })
+        .auth(username, authkey);
+    }
+    else{
+        result.error = true;
+        result.message = 'Session Id was not defined';
+        deferred.fulfill(result);
+    }
+
+    return deferred.promise;
+}
+
+//Call API to get a snapshot 
+function takeSnapshot() {
+
+    //webdriver has built-in promise to use
+    var deferred = webdriver.promise.defer();
+    var result = { error: false, message: null }
+    
+    if (sessionId){
+
+       
+        request.post(
+            'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId + '/snapshots', 
+            function(error, response, body) {
+                if (error) {
+                    result.error = true;
+                    result.message = error;
+                }
+                else if (response.statusCode !== 200){
+                    result.error = true;
+                    result.message = body;
+                }
+                else{
+                    result.error = false;
+                    result.message = 'success';
+                }
+                //console.log('fulfilling promise in takeSnapshot')
+                deferred.fulfill(result);
+            }
+        )
+        .auth(username,authkey);
+        
+    }
+    else{
+        result.error = true;
+        result.message = 'Session Id was not defined';
+        deferred.fulfill(result); //never call reject as we don't need this to actually stop the test
+    }
+
+    return deferred.promise;
+}
+
+//general error catching function
+function webdriverErrorHandler(err){
+
+    console.error('There was an unhandled exception! ' + err);
+
+    //if we had a session, end it and mark failed
+    if (driver && sessionId){
+        driver.quit();
+        setScore('fail').then(function(result){
+            console.log('set score to fail')
+        })
+    }
+}
