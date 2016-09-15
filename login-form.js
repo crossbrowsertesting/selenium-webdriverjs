3602feefd0f895cb9b5509a357a022a2af187dd2
Luke Kende <luke.kende@gmail.com>
2016-09-15 17:05:30 -0600

    more consistency fixes

--- login-form.js
+++ login-form.js
@@ -10,15 +10,15 @@ var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
 var username = 'user@email.com'; //replace with your email address 
 var authkey = '12345'; //replace with your authkey  
 
-var capabilities = {
+var caps = {
     name : 'Login Form Example',
     build :  '1.0',
-    browser_api_name : 'Chrome53', 
-    os_api_name : 'Win8', 
+    browser_api_name : 'IE10', 
+    os_api_name : 'Win7x64-C2', 
     screen_resolution : '1024x768',
-    record_video : "true",
-    record_network : "true",
-    browserName : "chrome", // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
+    record_video : 'true',
+    record_network : 'true',
+    browserName : 'internet explorer', // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
     username : username,
     password : authkey
 };
--- todo-app.js
+++ todo-app.js
@@ -10,18 +10,18 @@ var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
 var username = 'user@email.com'; //replace with your email address 
 var authkey = '12345'; //replace with your authkey 
 
-var capabilities = {
-    'name': 'Todo App Example',
-    'build': '1.0',
-    'os_api_name': 'Win7x64-C2',
-    'browser_api_name': 'IE10',
-    'screen_resolution': '1024x768',
-    'record_video': 'true',
-    'record_network': 'true',
-    'browserName': 'internet explorer',
-    'username': username,
-    'password': authkey
-}
+var caps = {
+    name : 'Todo App Example',
+    build :  '1.0',
+    browser_api_name : 'IE10', 
+    os_api_name : 'Win7x64-C2', 
+    screen_resolution : '1024x768',
+    record_video : 'true',
+    record_network : 'true',
+    browserName : 'internet explorer', // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
+    username : username,
+    password : authkey
+};
 
 var sessionId = null;
 
