525be8d0b0e469afdc55c4f6b4292fef3ef2d638
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-03-01 12:55:45 -0600

    Update basic.js

--- basic.js
+++ basic.js
@@ -5,8 +5,8 @@
 var webdriver = require('selenium-webdriver');
 var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 var request = require('request');
-var assert = require('selenium-webdriver/testing/assert');
 var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
+var assert = require('assert')
 
 var username = 'user@email.com'; //replace with your email address 
 var authkey = '12345'; //replace with your authkey 
@@ -14,45 +14,69 @@ var authkey = '12345'; //replace with your authkey
 var caps = {
     name : 'Basic Example',
     build :  '1.0',
-    browser_api_name : 'IE10', 
-    os_api_name : 'Win7x64-C2', 
-    screen_resolution : '1024x768',
+    version : '72', 
+    platform : 'Windows 10', 
+    screen_resolution : '1366x768',
     record_video : 'true',
-    record_network : 'true',
-    browserName : 'internet explorer', // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
+    record_network : 'false',
+    browserName : 'Chrome',
     username : username,
     password : authkey
 };
 
 var sessionId = null;
 
-//register general error handler
-webdriver.promise.controlFlow().on('uncaughtException', webdriverErrorHandler);
- 
-var driver = new webdriver.Builder()
-    .usingServer(remoteHub)
-    .withCapabilities(caps)
-    .build();
 
-driver.getSession().then(function(session){
-    sessionId = session.id_; //need for API calls
-});
-driver.get('http://crossbrowsertesting.github.io/selenium_example_page.html');
-driver.getTitle().then(function(title){
-    console.log('page title is ', title);
-    assert(title).equals('Selenium Test Example Page');
-});
+console.log('Connection to the CrossBrowserTesting remote server');
+async function full(){
+    try{
+    var driver = new webdriver.Builder()
+                .usingServer(remoteHub)
+                .withCapabilities(caps)
+                .build();
 
-driver.quit();
 
-//set the score as passing
-driver.call(setScore, null, 'pass');
+    console.log('Waiting on the browser to be launched and the session to start');
+
+    await driver.getSession().then(function(session){
+        sessionId = session.id_; //need for API calls
+        console.log('Session ID: ', sessionId); 
+        console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
+    });
+
+    //load your URL
+    await driver.get('http://crossbrowsertesting.github.io/selenium_example_page.html');
+
+    //take snapshot via cbt api
+    await driver.takeSnapshot();
+    
+    //check title
+    await driver.getTitle().then(function(title){
+        console.log('page title is ', title);
+        assert.equal(title, 'Selenium Test Example Page');
+    });
+
+
+    //quit the driver
+    await driver.quit()
+
+    //set the score as passing
+    setScore('pass').then(function(result){
+        console.log('SUCCESS! set score to pass')
+    });
+    }
+    catch(e){
+        webdriverErrorHandler(e, driver)
+    }
+   
+}
+
+full();
+
 
 //Call API to set the score
-function setScore(score) {
-
-    //webdriver has built-in promise to use
-    var deferred = webdriver.promise.defer();
+function setScore(score){
+    return new Promise((resolve, fulfill)=> {
     var result = { error: false, message: null }
 
     if (sessionId){
@@ -76,8 +100,6 @@ function setScore(score) {
                 result.error = false;
                 result.message = 'success';
             }
-
-            deferred.fulfill(result);
         })
         .auth(username, authkey);
     }
@@ -87,19 +109,58 @@ function setScore(score) {
         deferred.fulfill(result);
     }
 
-    return deferred.promise;
+    
+        result.error ? fulfill('Fail') : resolve('Pass');
+    });
+}
+
+//Call API to get a snapshot 
+webdriver.WebDriver.prototype.takeSnapshot = function() {
+
+    return new Promise((resolve, fulfill)=> { 
+        var result = { error: false, message: null }
+        
+        if (sessionId){
+            request.post(
+                'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId + '/snapshots', 
+                function(error, response, body) {
+                    if (error) {
+                        result.error = true;
+                        result.message = error;
+                    }
+                    else if (response.statusCode !== 200){
+                        result.error = true;
+                        result.message = body;
+                    }
+                    else{
+                        result.error = false;
+                        result.message = 'success';
+                    }
+                }
+            )
+            .auth(username,authkey);
+            
+        }
+        else{
+            result.error = true;
+            result.message = 'Session Id was not defined';
+           
+        }
+
+            result.error ? fulfill('Fail') : resolve('Pass'); //never call reject as we don't need this to actually stop the test
+    });
 }
 
 //general error catching function
-function webdriverErrorHandler(err){
+function webdriverErrorHandler(err, driver){
 
-    console.error('There was an unhandled exception! ' + err);
+    console.error('There was an unhandled exception! ' + err.message);
 
     //if we had a session, end it and mark failed
     if (driver && sessionId){
         driver.quit();
         setScore('fail').then(function(result){
-            console.log('set score to fail')
+            console.log('FAILURE! set score to fail')
         })
     }
 }
